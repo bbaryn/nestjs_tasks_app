c9928941e993692a7ff897b37c0d14b8788a56ce
Bartłomiej Baryń <b.baryn@moodup.team>
2019-10-16 22:35:14 +0200

    Create getById endpoint

--- src/tasks/data/services/tasks.service.ts
+++ src/tasks/data/services/tasks.service.ts
@@ -1,6 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TaskRepository } from '../dao/taskDao';
+import { Task } from '../entity/task.entity';
 
 @Injectable()
 export class TasksService {
+    constructor(
+        @InjectRepository(TaskRepository)
+        private taskRepository: TaskRepository,
+    ) {}
 
+    async getTaskById(id: number): Promise<Task> {
+        const found = await this.taskRepository.findOne(id);
+
+        if(!found) {
+            throw new NotFoundException(`Task with ID "${id}" doesn't exist"`);
+        }
+
+        return found;
+    }
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { TasksService } from './data/services/tasks.service';
+import { Task } from './data/entity/task.entity';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) {}
 
+    @Get('/:id')
+    getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+        return this.tasksService.getTaskById(id)
+    }
 }
